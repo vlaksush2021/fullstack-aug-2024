4e9ab82af678490ec2aff5f3ed4aff56068e4354
Suresh V <vlaksush@outlook.com>
2024-09-29 14:51:04 +0530

    demo added

--- react.js/demos/demo1/hello-react/src/App.js
+++ react.js/demos/demo1/hello-react/src/App.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Greeting from './Greeting';
 import Product from './Product';
 import Counter from './Counter';
 import InputFiled from './InputFiled';
+import Employee from './day2/Employee';
+import Student from './day2/Student';
 function App() {
   const name = "Shiva";
   const product = {
@@ -11,16 +13,36 @@ function App() {
   }
   const org = "ABC Products";
 
+  const [empCount,setEmpCount] = useState(0);
+  const [studCount,setstudCount] = useState(0);
+
+  const empCountHandler = (data) => { //1
+    setEmpCount(data);
+  }
+
+  const studCountHandler = (data) => { //1
+    setstudCount(data);
+  }
+
   return (
     <div style={{ textAlign: 'left'}}>
       <h1>Learning React </h1>
       <hr color='red' />
-  
 
-      <Greeting name={name}  />  
-      <Product product={product} org={org} />
-      <Counter />
-      <InputFiled />
+      <h3>Employee Registration - {empCount} </h3>
+      <h3>Student  Registration - {studCount} </h3>
+      <h3>Total Registration - {empCount+studCount}</h3>
+
+      <hr />
+      <table width="100%">
+        <tr>
+          <td><Employee empCount = {empCount} empHandler={empCountHandler} /></td>
+          <td><Student studCount = {studCount} studHandler={studCountHandler} /></td>
+        </tr>
+      </table>
+      
+     
+
     </div>
   );
 }
