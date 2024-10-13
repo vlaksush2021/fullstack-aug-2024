35a636ab1eb776e1735d51591cc9a9675747c0e8
Suresh V <vlaksush@outlook.com>
2024-10-13 16:57:02 +0530

    updated day5 react session

--- react.js/demos/demo1/hello-react/src/App.js
+++ react.js/demos/demo1/hello-react/src/App.js
@@ -279,10 +279,252 @@
 
 
 
-//Day5 - React Router
-import React from 'react';
-import {Routes, Route, BrowserRouter as Router, Link} from 'react-router-dom';
-import Help from './Router-Demos/Help';
+// //Day5 - React Router
+// import React from 'react';
+// import {Routes, Route, BrowserRouter as Router, Link} from 'react-router-dom';
+// import Help from './Router-Demos/Help';
+
+// const Home = () => {
+//   return (
+//     <div>
+//       <h3>Home Page</h3>
+//     </div>
+//   )
+// }
+
+// const About = () => {
+//   return (
+//     <div>
+//       <h3>Aboutus Page</h3>
+//     </div>
+//   )
+// }
+
+// const Contact = () => {
+//   return (
+//     <div>
+//       <h3>Contactus Page</h3>
+//     </div>
+//   )
+// }
+
+// function App() {
+//   return (
+//     <Router>
+//     <div style={{ textAlign: 'left' }}>
+//       <h1>Learning React </h1>
+//       <div align="right">
+//         <Link to="/">Home</Link> | 
+//         <Link to="/about">About us</Link> | 
+//         <Link to="/about">Contat us</Link> |
+//         <Link to="/help">Help</Link>
+//       </div>
+//       <hr color="red" />
+
+//       <Routes>
+//         <Route path="/" element={<Home />}  />
+//         <Route  path="/about" element={<About />} />
+//         <Route path="/contact" element={<Contact />} />
+//         <Route path="/help" element={<Help />} />
+//       </Routes>
+
+
+//     </div>
+//     </Router>
+//   );
+// }
+
+// export default App;
+
+
+
+
+// //Lab09 - Reach Router
+// import React from 'react';
+// import { Router, Link, useNavigate } from '@reach/router';
+
+// const Home = () => {
+//   return (
+//     <div>
+//       <h3>Home Page</h3>
+//     </div>
+//   )
+// }
+
+// const Products = () => {
+
+//   const navigate = useNavigate();
+
+//   const addToCart = () => {
+//     alert("Product added to cart!")
+//     navigate("/cart")
+//   }
+
+//   return (
+//     <div>
+//       <h3>Products Page</h3>
+//       <button onClick={addToCart}>Add Product to Cart</button>
+//     </div>
+//   )
+// }
+
+// const Cart = () => {
+//   return (
+//     <div>
+//       <h3>Cart Page</h3>
+//     </div>
+//   )
+// }
+
+// const NotFound = () => {
+//   return (
+//     <div>
+//       <h3>Invalid Option</h3>
+//     </div>
+//   )
+// }
+
+// function App() {
+//   return (
+
+//     <div style={{ textAlign: 'left' }}>
+//       <h1>Learning React </h1>
+
+//       <div align="right">
+//         <Link to="/">Home</Link> |
+//         <Link to="/products">Products</Link> |
+//         <Link to="/cart">Cart</Link>
+//       </div>
+
+//       <hr color="red" />
+//       <Router>
+//         <Home path="/" />
+//         <Products path="/products" />
+//         <Cart path="/cart" />
+//         <NotFound default />
+//       </Router>
+
+
+//     </div>
+
+//   );
+// }
+
+// export default App;
+
+
+
+
+
+// //Dynamic Route with Parameters and nesting routes
+// import React from 'react';
+// import { Router, Link } from '@reach/router';
+
+// const Home = () => {
+//   return (
+//     <div>
+//       <h3>Home Page</h3>
+//     </div>
+//   )
+// }
+
+
+// const UserProfile = ({ id, children }) => {
+//   const userData = {
+//     1: { name: 'bala', age: 28 },
+//     2: { name: 'peter', age: 34 }
+//   }
+//   const user = userData[id] || { name: 'Unknown', age: 'N/A' }
+
+//   return (
+//     <div>
+//       <h2>User Profile - {user.name}</h2>
+//       <p>Age : {user.age}</p>
+
+//       <nav>
+//         |
+//         <Link to="details">Details</Link> |
+//         <Link to="posts">Posts</Link>
+//         |
+//       </nav>
+
+//       {/* <Router>
+//         <UserDetails path="/details"></UserDetails>
+//         <UserPosts path="/posts"></UserPosts>
+//       </Router> */}
+
+//       {children}
+
+//     </div>
+//   )
+// }
+
+// const UserDetails = ({ id }) => {
+
+//   return (
+//     <div>
+//       <h3>Details for user {id} </h3>
+//       <p>This section contains additional info about the user.</p>
+//     </div>
+//   )
+// }
+
+
+// const UserPosts = ({ id }) => {
+
+//   return (
+//     <div>
+//       <h3>Posts for user {id} </h3>
+//       <p>This section contains additional info about the user posts.</p>
+//     </div>
+//   )
+// }
+
+
+// const NotFound = () => {
+//   return (
+//     <div>
+//       <h3>Invalid Option</h3>
+//     </div>
+//   )
+// }
+
+// function App() {
+//   return (
+
+//     <div style={{ textAlign: 'left' }}>
+//       <h1>Learning React </h1>
+
+//       <div align="right">
+//         |
+//         <Link to="/">Home</Link> |
+//         <Link to="/user/1">User1 Profile</Link> |
+//         <Link to="/user/2">User2 Profile</Link>
+//         |
+
+
+//       </div>
+
+//       <hr color="red" />
+//       <Router>
+//         <Home path="/" />
+//         <UserProfile path="/user/:id">
+//           <UserDetails path="details" />
+//           <UserPosts path="posts" />
+//         </UserProfile>
+//         <NotFound default />
+//       </Router>
+//     </div>
+
+//   );
+// }
+
+// export default App;
+
+
+//API Integrationand Handling different States
+import React, { useState, useEffect } from 'react';
+import { Router, Link } from '@reach/router';
 
 const Home = () => {
   return (
@@ -292,45 +534,64 @@ const Home = () => {
   )
 }
 
-const About = () => {
+const Posts = () => {
+
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(res => res.json())
+      .then(res => {
+        setPosts(res)
+        setLoading(false);
+      }
+      )
+      .catch(err => setError(err))
+  })
+
+  if (loading) return <p>Loading Posts</p>
+
+  if (error)
+    return <p>Error : {error} </p>
+
   return (
     <div>
-      <h3>Aboutus Page</h3>
+      <h3>Posts</h3>
+
+      <ul>
+        {
+          posts.map((post) => {
+            return <li key={post.id}>
+              {post.title}
+            </li>
+          })
+        }
+      </ul>
     </div>
   )
 }
 
-const Contact = () => {
-  return (
-    <div>
-      <h3>Contactus Page</h3>
-    </div>
-  )
-}
 
 function App() {
   return (
-    <Router>
+
     <div style={{ textAlign: 'left' }}>
       <h1>Learning React </h1>
+
       <div align="right">
-        <Link to="/">Home</Link> | 
-        <Link to="/about">About us</Link> | 
-        <Link to="/about">Contat us</Link> |
-        <Link to="/help">Help</Link>
+        <Link to="/" >Home</Link> |
+        <Link to="/posts" > Posts</Link>
       </div>
+
       <hr color="red" />
-
-      <Routes>
-        <Route path="/" element={<Home />}  />
-        <Route  path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/help" element={<Help />} />
-      </Routes>
-
-
+      <Router>
+        <Home path="/" />
+        <Posts path="/posts" />
+      </Router>
     </div>
-    </Router>
+
   );
 }
 
